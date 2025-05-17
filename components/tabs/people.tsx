29122c958c48ebd724476ca2fc94c6b1db19dae0
Johannes Klaus <johannes.k-buero@gmx.de>
2025-05-17 02:53:24 +0200

    handling Favouriten add people to group

--- components/tabs/people.tsx
+++ components/tabs/people.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { FlatList } from 'react-native'
-import { Text, YStack, Image, XStack } from 'tamagui'
+import { FlatList, Pressable } from 'react-native'
+import { Text, YStack, Image } from 'tamagui'
+import { useRouter } from 'expo-router'
 import { items } from '@/assets/dummydata/_data'
 
 const PeopleListingPage = () => {
+  const router = useRouter()
   const peopleItems = items.filter((item) => item.categoryId === 'people')
-
 
   return (
     <YStack flex={1} p="$4" space="$4" backgroundColor="#000">
@@ -18,29 +19,31 @@ const PeopleListingPage = () => {
         keyExtractor={(item) => item.id}
         ItemSeparatorComponent={() => <YStack height={12} />}
         renderItem={({ item }) => (
-          <YStack
-            flexDirection="row"
-            borderWidth={1}
-            borderColor="#333"
-            borderRadius={10}
-            overflow="hidden"
-            backgroundColor="#121212"
-            padding={8}
-            alignItems="center"
-          >
-            <Image
-              source={{ uri: item.image }}
-              alt={item.name}
-              height={80}
-              width={80}
+          <Pressable onPress={() => router.push(`/details/${item.id}`)}>
+            <YStack
+              flexDirection="row"
+              borderWidth={1}
+              borderColor="#333"
               borderRadius={10}
-              resizeMode="cover"
-              mr={12}
-            />
-            <Text fontWeight="600" fontSize="$4" color="#fff" flexShrink={1}>
-              {item.name}
-            </Text>
-          </YStack>
+              overflow="hidden"
+              backgroundColor="#121212"
+              padding={8}
+              alignItems="center"
+            >
+              <Image
+                source={{ uri: item.image }}
+                alt={item.name}
+                height={80}
+                width={80}
+                borderRadius={10}
+                resizeMode="cover"
+                mr={12}
+              />
+              <Text fontWeight="600" fontSize="$4" color="#fff" flexShrink={1}>
+                {item.name}
+              </Text>
+            </YStack>
+          </Pressable>
         )}
       />
     </YStack>
